d5b49bdf8d1ce4e7bdb3353f3e0a0736b431300b
FerNanda2580 <fernandafelix789@gmail.com>
2022-12-04 22:06:13 -0300

    History 1.0

---
+++ Personagens/src/Nevermore/Personagens.java
@@ -0,0 +1,16 @@
+package Nevermore;
+
+public class Personagens {
+
+   public String nome;
+   public String tipo;
+
+   Personagens(
+         String nome,
+         String tipo) {
+      this.nome = nome;
+      this.tipo = tipo;
+
+   }
+
+}
