c7a22be83ad0d459006fffd88cda3857c097926e
FerNanda2580 <fernandafelix789@gmail.com>
2022-12-09 22:23:45 -0300

    miniprojeto2

--- Personagens/src/Nevermore/App.java
+++ Personagens/src/Nevermore/App.java
@@ -5,61 +5,57 @@ import java.util.Scanner;
 public class App {
         public static void main(String[] args) throws Exception {
                 Scanner input = new Scanner(System.in);
-                Personagens wandinha = new Personagens("Wandinha", "Excluído");
-                Personagens tyler = new Personagens("Tyler", "Padrão");
-                Personagens xavier = new Personagens("Xavier", "Excluído");
-                Personagens bianca = new Personagens("Bianca", "Excluído");
-                Personagens enid = new Personagens("Enid", "Excluído");
-                Personagens ajax = new Personagens("Ajax", "Excluído");
+                Personagens wandinha = new Personagens(" Wandinha ", " Excluído ", 50);
+                Personagens tyler = new Personagens(" Tyler ", " Padrão ", 50);
+                Personagens xavier = new Personagens(" Xavier ", " Excluído ", 50);
+                Personagens bianca = new Personagens(" Bianca ", " Excluído ", 50);
+                Personagens enid = new Personagens(" Enid ", " Excluído ", 50);
+                Personagens ajax = new Personagens(" Ajax ", " Excluído ", 50);
 
                 System.out.println("A" + wandinha.nome
-                                + " parece ter um dom especial para se envolver em problemas, mas tudo e aceitável quando se está a procura da verdade.");
-                System.out.println("Então como ela poderia deixar que sua melhor amiga e colega de quarto " + enid.nome
-                                + " fosse acusada de envenenar " + xavier.nome
-                                + " injustamente, durante uma viagem com os seus amigos "
-                                + tyler.nome + ", " + bianca.nome + " e " + ajax.nome);
+                                + "parece ter um dom especial para se envolver em problemas, mas tudo é aceitável quando se está a procura da verdade.");
+                System.out.println("Então como ela poderia deixar que sua melhor amiga e colega de quarto" + enid.nome
+                                + "fosse acusada de envenenar" + xavier.nome
+                                + "injustamente, durante uma viagem com os seus amigos"
+                                + tyler.nome + "," + bianca.nome + "e" + ajax.nome);
                 System.out.println(
-                                "De volta a Nevermore ela começa a sua investigação para saber quem é o verdadeiro culpado pelo envenenamento de "
-                                                + xavier.nome + "por onde ela deveria começar a sua investogação ?");
+                                "De volta a Nevermore ela começa a sua investigação para saber quem é o verdadeiro culpado pelo envenenamento de"
+                                                + xavier.nome + "por onde ela deveria começar a sua investigação ?");
 
                 System.out.println("1- Identificar o veneno utilizado\n2- Refazer os passos de Xavier");
                 int first = input.nextInt();
 
                 if (first == 1) {
-                        System.out.println("Após olhar os documentos da autopsia " + wandinha.nome
-                                        + " descobre que o veneno utilizado veio de uma planta rara que a escola mantem na estufa, a estufa é vijiada por cameras mas as filmagens se encontram na sala da diretora que está sempre fechada.");
+                        System.out.println("Após olhar os documentos da autopsia" + wandinha.nome
+                                        + "descobre que o veneno utilizado veio de uma planta rara que a escola mantem na estufa, a estufa é vijiada por cameras mas as filmagens se encontram na sala da diretora que está sempre fechada.");
 
                         System.out.println(
                                         "1- Invadir a sala e correr o risco de ser expulsa. \n2- Não invadir a sala e procurar outra solução.");
                         int second = input.nextInt();
 
                         if (second == 1) {
-                                System.out.println(wandinha.nome + " Foi expulsa após invadir a sala da diretora e "
+                                System.out.println(wandinha.nome + "Foi expulsa após invadir a sala da diretora e"
                                                 + enid.nome
-                                                + " foi julgada culpada.");
+                                                + "foi julgada culpada.");
                         }
 
                         if (second == 2) {
-                                System.out.println(wandinha.nome + " decide então conversar com " + xavier.nome
-                                                + " que confirma que "
+                                System.out.println(wandinha.nome + "decide então conversar com" + xavier.nome
+                                                + "que confirma que"
                                                 + enid.nome
-                                                + " estava com ele na hora do envenenamento, com um novo halibi, "
+                                                + "estava com ele na hora do envenenamento, com um novo halibi,"
                                                 + enid.nome
-                                                + " é inocentada");
+                                                + "é inocentada");
                         }
                 }
 
                 if (first == 2) {
-                        System.out.println("Ao refazer os passos do " + xavier.nome + ", " + wandinha.nome
-                                        + " encontra uma pulseira do canto da manhã que pertence a " + bianca.nome
-                                        + ", "
-                                        + bianca.nome + " diz que deu a pulseira de presente para tayler");
-                        System.out.println("Ao vasculhar o quarto de " + tyler.nome
-                                        + "ela acha o veneno e entrega as autoridades, " + tyler.nome
-                                        + " é julgado culpado.");
+                        wandinha.alterarpontos(-50);
+                        System.out.println("Ao refazer os passos do" + xavier.nome + "," + wandinha.nome
+                                        + "encontra uma pulseira amaldiçoada e morre");
+
+                        input.close();
+
                 }
-
-                input.close();
-
         }
 }
--- Personagens/src/Nevermore/Personagens.java
+++ Personagens/src/Nevermore/Personagens.java
@@ -4,12 +4,36 @@ public class Personagens {
 
    public String nome;
    public String tipo;
+   int pontos;
 
    Personagens(
          String nome,
-         String tipo) {
+         String tipo,
+         int pontos) {
       this.nome = nome;
       this.tipo = tipo;
+      this.pontos = pontos;
+
+   }
+
+   void alterarpontos(int alteracao) {
+      if (alteracao > 0) {
+         this.pontos += alteracao;
+         System.out.println(this.nome + "ganhou" + alteracao + " pontos");
+      }
+
+      else if (alteracao < 0) {
+         (this.pontos) += alteracao;
+         System.out.println(this.nome + "perdeu " + alteracao * -1 + " pontos");
+      }
+
+      else if (this.pontos > 50) {
+         this.pontos = 50;
+      }
+
+      else if (this.pontos < 50) {
+         this.pontos = 50;
+      }
 
    }
 
